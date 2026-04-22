d29aab221f88c0f0761b400e711f7c5c19e92769
Noemi Cruces <mimicruces1902@gmail.com>
2026-04-21 20:49:41 -0600

    Se modifico para que se regresaran los datos del clima en formato de arreglo para facilidad en uso del frontend, tambien se modifico para que la fecha sea dinamica y se actualice al dia en que se hace la consulta, en los datos de retorno se agrego la fecha.

--- src/weather/weather.service.ts
+++ src/weather/weather.service.ts
@@ -8,10 +8,12 @@ import { fetchWeatherApi } from "openmeteo";
 
 @Injectable()
 export class WeatherService {
-  constructor(private http: HttpService) {}
+  constructor(private http: HttpService) { }
 
   async getWeather(lat: number, lon: number) {
-    
+
+    //const hoy = new Date().toISOString().split('T')[0]; Esta ño porque si es tarde da la fecha del dia siguiente
+    const hoy = new Date().toLocaleDateString('en-CA')
     const params = {
       latitude: lat,
       longitude: lon,
@@ -21,9 +23,9 @@ export class WeatherService {
       current: "temperature_2m",
 
       hourly: "temperature_2m,precipitation_probability,precipitation,rain,showers,weather_code,relative_humidity_2m",
-      
-      start_date: "2026-04-20",
-	    end_date: "2026-04-20",
+
+      start_date: hoy,
+      end_date:hoy,
       timezone: "auto"
     };
 
@@ -39,9 +41,17 @@ export class WeatherService {
 
     const daily = response.daily();
 
+    const horas = [
+      '0h', '1h', '2h', '3h', '4h', '5h',
+      '6h', '7h', '8h', '9h', '10h', '11h',
+      '12h', '13h', '14h', '15h', '16h', '17h',
+      '18h', '19h', '20h', '21h', '22h', '23h'
+    ];
+
     return {
-      latitude: response.latitude(),
-      longitude: response.longitude(),
+      date: hoy,
+      //latitude: response.latitude(),
+      //longitude: response.longitude(),
       //elevation: response.elevation(),
 
       current: {
@@ -49,22 +59,31 @@ export class WeatherService {
       },
       hourly: {
         //time: hourly?.time(),
+        //time: hourly?.time()?.toString(),
+        time: horas,
         //time: new Date(Number(hourly?.time()) * 1000).toISOString(),
-        temperature: hourly?.variables(0)?.valuesArray(),
+        /* temperature: hourly?.variables(0)?.valuesArray(),
         precipitation_probability: hourly?.variables(1)?.valuesArray(),
         precipitation: hourly?.variables(2)?.valuesArray(),
         rain: hourly?.variables(3)?.valuesArray(),
         showers: hourly?.variables(4)?.valuesArray(),
         weather_code: hourly?.variables(5)?.valuesArray(),
-        relative_humidity_2m: hourly?.variables(6)?.valuesArray(),
+        relative_humidity_2m: hourly?.variables(6)?.valuesArray(), */
+        temperature: Array.from(hourly?.variables(0)?.valuesArray() || []),
+        precipitation_probability: Array.from(hourly?.variables(1)?.valuesArray() || []),
+        precipitation: Array.from(hourly?.variables(2)?.valuesArray() || []),
+        rain: Array.from(hourly?.variables(3)?.valuesArray() || []),
+        showers: Array.from(hourly?.variables(4)?.valuesArray() || []),
+        weather_code: Array.from(hourly?.variables(5)?.valuesArray() || []),
+        relative_humidity_2m: Array.from(hourly?.variables(6)?.valuesArray() || []),
       },
       daily: {
-       // time: daily?.time(),
+        // time: daily?.time(),
         weather_code: daily?.variables(0)?.valuesArray(),
       },
     };
   }
-  
+
   create(createWeatherDto: CreateWeatherDto) {
     return 'This action adds a new weather';
   }
